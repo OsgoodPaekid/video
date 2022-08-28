33d0209179b05b249595e79f470da7e2a5d40fcb
Osgood <osgoodackom@gmail.com>
2022-08-28 11:18:11 +0000

    second commit

--- questions/setup/app.js
+++ questions/setup/app.js
@@ -1,2 +1,12 @@
 //using selectors inside the element
 // traversing the dom
+//const plusIcon = document.querySelector(".plus-icon")
+//const questionText = document.querySelector(".question-text")
+const btns = document.querySelectorAll(".question-btn")
+//const questionTitle = document.querySelector(".question-title")
+
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+        
+    })
+})
